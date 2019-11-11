a88f618fb854285fd5d2b4f94fc66bc769ea8b97
Vasyl Khrystynych <khrystynych.vasyl@gmail.com>
2019-11-12 00:52:39 +0200

    'test5'

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use('/',require('./routers/index.js'));
-app.use('/api',require('./routers/api.js'));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/',require('./routers/index.js'));
+// app.use('/api',require('./routers/api.js'));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT,function(err){
+app.use(express.static(__dirname + "/dist/littlestep"));
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname + '/dist/littlestep/index.html'))
+})
+
+app.listen(PORT, function(err) {
   if (err) {
     console.log(err);
   } else {
     console.log(`listen:${PORT}`);
   }
-})
+});
