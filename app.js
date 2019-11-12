4b7648540ef54f6d1266a9b894da0f6c3f824cd4
Vasyl Khrystynych <khrystynych.vasyl@gmail.com>
2019-11-12 07:44:57 +0200

    'test6'

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/dist/littlestep"));
 app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname + '/dist/littlestep/index.html'))
+  res.sendFile(path.join(__dirname + 'index.html'))
 })
 
 app.listen(PORT, function(err) {
